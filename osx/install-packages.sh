f168b7fc2275394be0a8d17ce1fd68842c0bdfda
Marco Reinwarth <marcoreinwarth@gmail.com>
2016-02-29 11:42:55 +0100

    update package managers and install packages

---
+++ osx/install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PORT_PACKAGES="htop watch"
+BREW_PACKAGES="git bash-completion"
+
+echo
+echo "Updating MacPorts"
+echo
+sudo port selfupdate
+sudo port upgrade outdated
+
+echo "Installing packages from MacPorts"
+echo
+echo "Install packages: ${PORT_PACKAGES}"
+echo
+sudo port install -y ${PORT_PACKAGES}
+
+echo
+echo "Updating HomeBrew"
+echo
+brew update
+brew upgrade
+
+echo
+echo "Installing packages from HomeBrew"
+echo
+echo "Install packages: ${BREW_PACKAGES}"
+echo
+brew install -y ${BREW_PACKAGES}
