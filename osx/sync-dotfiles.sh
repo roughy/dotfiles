b74473dde6519a0e8d3b1f19bad4eeda1ea72c0e
Marco Reinwarth <marcoreinwarth@gmail.com>
2016-04-30 11:38:26 +0200

    exlude new basic-setup file from sync

--- osx/sync-dotfiles.sh
+++ osx/sync-dotfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync*.sh" \
-      --exclude "install*.sh" --exclude "README.md" -avh --no-perms . ~;
+      --exclude "install*.sh" --exclude "basic-setup.sh" --exclude "README.md" \
+      -avh --no-perms . ~;
 
